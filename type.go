8b08ccb7ffa542f58d18a0264854b9e6ef5c492e
caselongo <caselongo@gmail.com>
2020-11-04 23:33:30 +0100

    x

--- type.go
+++ type.go
@@ -7,7 +7,11 @@ import (
 )
 
 type Type struct {
-	ID int `json:"id"`
+	ID    int    `json:"id"`
+	Slug  string `json:"slug"`
+	Title struct {
+		Rendered string `json:"rendered"`
+	} `json:"title"`
 }
 
 func (wp *WordPress) GetTypes(typeName string, typeID *int) (*[]Type, error) {
@@ -15,17 +19,35 @@ func (wp *WordPress) GetTypes(typeName string, typeID *int) (*[]Type, error) {
 		return nil, &types.ErrorString{"WordPress pointer is nil"}
 	}
 
+	perPage := 100
+	page := 1
+
+	types := []Type{}
+
 	urlString := fmt.Sprintf("%s/%s", wp.BaseURL(), typeName)
 	if typeID != nil {
 		urlString = fmt.Sprintf("%s/%v", urlString, *typeID)
 	}
-	fmt.Println(urlString)
 
-	types := []Type{}
+	for true {
+		types_ := []Type{}
 
-	err := wp.Get(urlString, &types)
-	if err != nil {
-		return nil, err
+		err := wp.Get(fmt.Sprintf("%s?per_page=%v&page=%v", urlString, perPage, page), &types_)
+		if err != nil {
+			return nil, err
+		}
+
+		types = append(types, types_...)
+
+		//if len(types_) < perPage {
+		//	break
+		//}
+
+		if len(types_) == 0 {
+			break
+		}
+
+		page++
 	}
 
 	return &types, nil
